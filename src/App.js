73c6b5a045a188772945f30511466d9c800f187e
Mateo Guisao Flórez <1039475395@itagui.loc>
2022-11-25 15:22:19 -0500

    consumo marvel api en react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./assets/logo.png";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Card } from "./components/Card";
+
+// https://gateway.marvel.com:443/v1/public/characters?apikey=788c8412ebf85f4bcc2e87b43c0b3ece
+
+// public key: 788c8412ebf85f4bcc2e87b43c0b3ece
+// private key: 77a51717ba4808f3b7e7b9ff27ff35c3a8d74f80
+// ts: 1
+
+// 177a51717ba4808f3b7e7b9ff27ff35c3a8d74f80788c8412ebf85f4bcc2e87b43c0b3ece
+
+// Hash: 3abdddf8b16f921bf669846b5e259bf2
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=788c8412ebf85f4bcc2e87b43c0b3ece&hash=3abdddf8b16f921bf669846b5e259bf2"
+      )
+      .then((res) => {
+        setCharacters(res.data.data.results);
+      })
+      .catch((error) => console.warn(error));
+  }, []);
+
+  console.log(characters);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex flex-col justify-center items-center">
+      <img src={logo} alt="" className="w-48" />
+
+      <div className="grid grid-cols-1 gap-3 lg:grid-cols-5 md:grid-cols-3">
+        {characters.map((per) => (
+          <Card
+            id={per.id}
+            photo={`${per.thumbnail.path}.${per.thumbnail.extension}`}
+            name={per.name}
+            desc={per.description}
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,22 @@
+// import App from '../App'
+
+export function Card({ photo, name, id, desc }) {
+  return (
+    <div className="w-full px-2" key={id}>
+      <div className="rounded overflow-hidden shadow-xl bg-gray-600 text-white hover:scale-105 hover:transition-transform hover:delay-100">
+        <img
+          className="h-80 w-full"
+          src={photo}
+          alt=""
+          loading="lazy"
+        ></img>
+        <div className="px-6 py-4 text-center">
+          <div className="font-bold text-xl mb-2 text-center">{name}</div>
+          <p>
+            {desc}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
