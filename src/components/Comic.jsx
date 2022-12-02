b705f09215a8d4c33b7ea36e1993cfe2b2bdc442
Mateo Guisao Flórez <1039475395@itagui.loc>
2022-12-02 14:38:40 -0500

    Delete div

--- src/components/Comic.jsx
+++ src/components/Comic.jsx
@@ -22,15 +22,6 @@ export function Comic() {
 
   return (
     <>
-      <div className="flex h-5/6 w-full justify-center">
-        <div className="trapecio w-full flex justify-center items-center z-10">
-          <img src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/mulogo_lob_log_eye_01.png" alt="" className="mt-52"/>
-          <h1 className="text-white">AVAILABLE NOW</h1>
-        </div>
-        <div className="section_image -z-10 absolute 2xl:opacity-70 overflow-hidden">
-          <img src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/newtomu_base_set_dsk_relaunch.jpg" alt="" className=""/>
-        </div>
-      </div>
       <div className="App flex flex-row justify-center items-center bg-white p-3 w-4/6 md:-mt-16 z-10">
         <div className="grid grid-cols-2 lg:grid-cols-4 md:grid-cols-2 gap-2">
           {comics.slice(0, 4).map((comic) => (
